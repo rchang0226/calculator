5ffd6d9eb6c55106c945bd7e31414dd1b81acf95
Raymond Chang <rchang0226@gmail.com>
2022-07-11 16:34:44 -0400

    Add calculator functionality via console logs

--- app.js
+++ app.js
@@ -18,4 +18,47 @@ function operate(a, op, b) {
     return op(a, b);
 }
 
+let a = null;
+let op = null;
+let b = null;
 
+const buttons = document.querySelectorAll('.number');
+buttons.forEach(button => button.addEventListener('click', handleNumbers));
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', handleOperator));
+const equal = document.getElementById('equals');
+equal.addEventListener('click', handleEquals);
+
+function handleNumbers(e) {
+    if (!op) {
+        if (!a) {a = this.id;}
+        else {a += this.id;}
+        console.log(a);
+    }
+    else {
+        if (!b) {b = this.id;}
+        else {b += this.id;}
+        console.log(b);
+    }
+}
+
+function handleOperator(e) {
+    if (!a) return;
+    if (b) operate(a, op, b);
+    switch (this.id) {
+        case "plus": op = add; break;
+        case "minus": op = subtract; break;
+        case "multiply": op = multiply; break;
+        case "divide": op = divide;
+    }
+    console.log(op);
+}
+
+function handleEquals(e) {
+    if (a && op && b) {
+        a = String(operate(Number(a), op, Number(b)));
+        op = null;
+        b = null;
+        console.log(a);
+    }
+}
